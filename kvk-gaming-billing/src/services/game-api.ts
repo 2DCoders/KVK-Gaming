0e66413e47b1e81427604260af8e4b00bc21fb68
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2026-09-15 15:27:34 +0530

    jhfdg

---
+++ kvk-gaming-billing/src/services/game-api.ts
@@ -0,0 +1,54 @@
+import axios from "axios";
+import { getEnv } from "@/env";
+
+const { API_URL } = getEnv();
+const GAMES_API_URL = `${API_URL}gaming-m/games/`; 
+
+const getToken = () => {
+  const cashier = localStorage.getItem("cashier")
+    ? JSON.parse(localStorage.getItem("cashier") as string)
+    : null;
+
+  return cashier ? cashier.token : null;
+};
+
+export const getGames = async () => {
+  try {
+    const response = await axios.get(`${GAMES_API_URL}`, {
+        headers: {
+            Authorization: `Bearer ${getToken()}`,
+        },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const createGame = async (gameData: FormData) => {
+    try {
+        const response = await axios.post(`${GAMES_API_URL}`, gameData, {
+            headers: {
+                Authorization: `Bearer ${getToken()}`,
+                "Content-Type": "multipart/form-data",
+            },
+        });
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const updateGame = async (gameData: FormData) => {
+    try {
+        const response = await axios.put(`${GAMES_API_URL}`, gameData, {
+            headers: {
+                Authorization: `Bearer ${getToken()}`,
+                "Content-Type": "multipart/form-data",
+            },
+        });
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
